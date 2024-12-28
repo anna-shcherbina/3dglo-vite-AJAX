27f7ccc8be63a6d3c9116a38954eb1832f2c2b54
Anna Shcherbinina <genacheb60@gmail.com>
2024-12-28 15:53:23 +0700

    done lesson 26

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-fetch('d.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    })
+const urlGet = 'db.json';
+const urlSend = 'https://jsonplaceholder.typicode.com/posts';
 
+const getData = (url) => { //получает данные из файла db.json
+    return fetch(url)
+        .then(response => response.json()) //методом response раскрыли данные (ответ от сервера), объект Response
+        .catch(error => console.log(error))
+};
+
+const sendData = (url, data) => { //отправляем полученные данные на адрес https://jsoт...
+    return fetch(url, {
+        method: 'POST', //POST отправляет данные
+        body: JSON.stringify(data), //тело - данные, что отправляем
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(response => response.json())
+        .catch(error => console.log(error))
+};
+
+const result = getData(urlGet); //заносим рез-т работы ф-ции в переменную
+
+sendData(urlSend, result) //вызываем ф-цию отправки данных 
+    .then(data => console.log(data)) //получаем данные в удоб.виде (должен быть статус 101)
+    .catch(error => console.log(error));
